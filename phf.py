0c4060d3860ca5a491cb7f11680ede8389e0c2ae
Armandov <armandove@live.com>
2025-01-29 16:21:44 -0600

    phone

---
+++ phf.py
@@ -0,0 +1,26 @@
+import phonenumbers
+from phonenumbers import timezone
+from phonenumbers import geocoder
+from phonenumbers import carrier
+
+#enter phone number along with country code
+#Example:+91 8897909599
+number = input("Enter phone number with country code : ")
+ 
+# Parsing String to the Phone number
+phoneNumber = phonenumbers.parse(number)
+ 
+# printing the timezone using the timezone module
+timeZone = timezone.time_zones_for_number(phoneNumber)
+print("timezone : "+str(timeZone))
+ 
+# printing the geolocation of the given number usi
+# ng the geocoder module
+geolocation = geocoder.description_for_number(phoneNumber,"en")
+print("location : "+geolocation)
+ 
+# printing the service provider name using the carrier module
+service = carrier.name_for_number(phoneNumber,"en")
+print("service provider : "+service)
+
+
